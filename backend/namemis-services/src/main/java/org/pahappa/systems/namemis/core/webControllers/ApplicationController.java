cdb8d6706b1073bf223ba0b3474ac251e9e6b4ef
Raymond Nsubuga <96862761+r-nsubuga@users.noreply.github.com>
2023-08-22 15:28:14 +0300

    Delete backend/namemis-services directory

--- backend/namemis-services/src/main/java/org/pahappa/systems/namemis/core/webControllers/ApplicationController.java
+++
@@ -1,9 +0,0 @@
-package org.pahappa.systems.namemis.core.webControllers;
-
-import org.sers.webutils.server.web.controllers.AbstractApplicationController;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ApplicationController extends AbstractApplicationController {
-
-}
